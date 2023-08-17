326c92c731a0c0352a614f63e479a7f2b73556fc
Anant Soft Computing <anantsoftcomputing@gmail.com>
2023-08-17 12:15:17 +0530

    Update settings.py

--- newsite/newsite/settings.py
+++ newsite/newsite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
